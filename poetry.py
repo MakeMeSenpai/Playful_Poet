40d476c6c70568ffa53708821a3695a42521cc6a
Anthony <sean.a.protho1@gmail.com>
2019-11-04 16:31:52 -0800

    skrew strech challenge

--- poetry.py
+++ poetry.py
@@ -44,24 +44,6 @@ def lines_printed_random():
         print("Rand Lines: " + poem[num])
         #becuase of num, we are able to go threw the list randomly, and select pecifically chosen lines by num. Who knew he could be such a poet
 
-def words_printed_random():
-    #this is me defining lines_printed_random
-    poem = "If you are a dreamer, come in,#If you are a dreamer, a wisher, a liar,#A hope-er, a pray-er, a magic bean buyer…#If you’re a pretender, come sit by my fire#For we have some flax-golden tales to spin.#Come in!#Come in!"
-    #long time no see. poem says hello!
-    poem = poem.split(" ")
-    #this splits her into words in a list
-    poem = " ".join(poem)
-    #this joins her back together for later splitting
-    poem = poem.split("#")
-    #she is now seperated by lines
-    poem = " ".join(poem)
-    #totally not the same as the last function
-    for _ in poem:
-        #this for loop looks threw each item in the list poem
-        num = random.randint(0, len(poem) - 1)
-        #num is now a randim number generator. Thank him!
-        print("Rand Words: " + poem[num])
-        #becuase of num, we are able to go threw the list randomly, and select pecifically chosen lines by num. Who knew he could be such a poet
 
 def rev_poetry():
     #this is me defining rev_poetry (aka my custom function)
